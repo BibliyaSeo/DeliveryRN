093294eeb52e652abc8162f8af394ff6d76b4838
BibliyaSeo <seongkyeong.seo@gmail.com>
2022-11-29 09:54:41 +0900

    Feat: react-navigation 추가

---
+++ screens/HomeScreen.tsx
@@ -0,0 +1,37 @@
+import {useNavigation} from '@react-navigation/native';
+import React, {useLayoutEffect} from 'react';
+import {Image, SafeAreaView, Text, View} from 'react-native';
+import {ChevronDownIcon} from 'react-native-heroicons/outline';
+
+export default function HomeScreen() {
+  const navigation = useNavigation();
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, []);
+
+  return (
+    <SafeAreaView>
+      <Text className="text-red-500">
+        {/* Header */}
+        <View className="flex-row pb-3 items-center mx-4 space-x-2">
+          <Image
+            source={{uri: 'https://links.papareact.com/wru'}}
+            className="h-7 w-7 bg-gray-300 p-4 rounded-full"
+          />
+          <View>
+            <Text className="font-bold text-gray-400 text-xs">
+              Deliver Now!
+            </Text>
+            <Text className="font-bold text-xl">
+              Current Location
+              <ChevronDownIcon size={20} color="#00CCBB" />
+            </Text>
+          </View>
+        </View>
+      </Text>
+    </SafeAreaView>
+  );
+}
